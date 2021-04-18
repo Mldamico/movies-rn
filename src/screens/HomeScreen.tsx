d3fec257dc8f3086c140f61a347029240540f7a8
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-18 12:12:24 -0300

    Movie Interface

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -2,10 +2,11 @@ import React from 'react';
 import {useEffect} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import movieDB from '../api/movieDB';
+import {MovieDBNowPlaying} from '../interfaces/movieInterface';
 
 export const HomeScreen = () => {
   useEffect(() => {
-    movieDB.get('/now_playing').then(resp => {});
+    movieDB.get<MovieDBNowPlaying>('/now_playing').then(resp => {});
   }, []);
   return (
     <View>
