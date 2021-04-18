acaf3a84e7e40ef3c36ee0504fc8f2eb3eb839e9
matias.lioneldamico@gmail.com <matias.lioneldamico@gmail.com>
2021-04-18 15:42:16 -0300

    Poster

---
+++ src/components/MoviePoster.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import {Image} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
+import {Movie} from '../interfaces/movieInterface';
+
+interface Props {
+  movie: Movie;
+}
+
+export const MoviePoster = ({movie}: Props) => {
+  const uri = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+  return (
+    <View style={{width: 300, height: 420}}>
+      <View style={styles.imageContainer}>
+        <Image style={styles.image} source={{uri}} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  image: {
+    flex: 1,
+    borderRadius: 18,
+  },
+  imageContainer: {
+    flex: 1,
+    borderRadius: 18,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 10,
+    },
+    shadowOpacity: 0.24,
+    shadowRadius: 7,
+    elevation: 10,
+  },
+});
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import {ActivityIndicator, StyleSheet, Text, View} from 'react-native';
 import {useMovies} from '../hooks/useMovies';
+import {MoviePoster} from '../components/MoviePoster';
+import {useSafeAreaInsets} from 'react-native-safe-area-context';
 
 export const HomeScreen = () => {
   const {movies, loading} = useMovies();
+  const {top} = useSafeAreaInsets();
   if (loading) {
     return (
       <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
@@ -12,8 +15,8 @@ export const HomeScreen = () => {
     );
   }
   return (
-    <View>
-      <Text>Home Screen</Text>
+    <View style={{marginTop: top}}>
+      <MoviePoster movie={movies[0]} />
     </View>
   );
 };
